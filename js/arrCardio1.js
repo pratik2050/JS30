15127ee0c6296ea2b398882dc6677d792b3e1d02
Pratik Das <67233000+pratik2050@users.noreply.github.com>
2021-12-21 20:59:54 +0530

    array_1-final

---
+++ js/arrCardio1.js
@@ -0,0 +1,152 @@
+const inventors = [
+  { first: "Albert", last: "Einstein", year: 1879, passed: 1955 },
+  { first: "Isaac", last: "Newton", year: 1643, passed: 1727 },
+  { first: "Galileo", last: "Galilei", year: 1564, passed: 1642 },
+  { first: "Marie", last: "Curie", year: 1867, passed: 1934 },
+  { first: "Johannes", last: "Kepler", year: 1571, passed: 1630 },
+  { first: "Nicolaus", last: "Copernicus", year: 1473, passed: 1543 },
+  { first: "Max", last: "Planck", year: 1858, passed: 1947 },
+  { first: "Katherine", last: "Blodgett", year: 1898, passed: 1979 },
+  { first: "Ada", last: "Lovelace", year: 1815, passed: 1852 },
+  { first: "Sarah E.", last: "Goode", year: 1855, passed: 1905 },
+  { first: "Lise", last: "Meitner", year: 1878, passed: 1968 },
+  { first: "Hanna", last: "Hammarström", year: 1829, passed: 1909 },
+];
+
+const people = [
+  "Bernhard, Sandra",
+  "Bethea, Erin",
+  "Becker, Carl",
+  "Bentsen, Lloyd",
+  "Beckett, Samuel",
+  "Blake, William",
+  "Berger, Ric",
+  "Beddoes, Mick",
+  "Beethoven, Ludwig",
+  "Belloc, Hilaire",
+  "Begin, Menachem",
+  "Bellow, Saul",
+  "Benchley, Robert",
+  "Blair, Robert",
+  "Benenson, Peter",
+  "Benjamin, Walter",
+  "Berlin, Irving",
+  "Benn, Tony",
+  "Benson, Leana",
+  "Bent, Silas",
+  "Berle, Milton",
+  "Berry, Halle",
+  "Biko, Steve",
+  "Beck, Glenn",
+  "Bergman, Ingmar",
+  "Black, Elk",
+  "Berio, Luciano",
+  "Berne, Eric",
+  "Berra, Yogi",
+  "Berry, Wendell",
+  "Bevan, Aneurin",
+  "Ben-Gurion, David",
+  "Bevel, Ken",
+  "Biden, Joseph",
+  "Bennington, Chester",
+  "Bierce, Ambrose",
+  "Billings, Josh",
+  "Birrell, Augustine",
+  "Blair, Tony",
+  "Beecher, Henry",
+  "Biondo, Frank",
+];
+
+// Array Filter()
+//Filter List of inventors born 1500
+const fifteen = inventors.filter(function (inventor) {
+  if (inventor.year >= 1500 && inventor.year < 1600) {
+    return true;
+  }
+
+  //we can use inventor => (inventor.year >= 1500 && inventor.year <1600)
+});
+console.table(fifteen);
+
+//Array map()
+//Array of inventorsx first and last name
+const fullName = inventors.map(function (inventor) {
+  return `${inventor.first} ${inventor.last}`;
+});
+console.log(fullName);
+
+//Array sort()
+//Sort the inventors by birthdate
+const ordered = inventors.sort(function (a, b) {
+  if (a.year > b.year) {
+    return 1;
+  } else {
+    return -1;
+  }
+  // we can also use const ordered = inventors.sort((a,b) => a.year > b.year ?  1 : -1 );
+});
+console.table(ordered);
+
+//Array Reduce()
+//How many years does all invertors live
+const totalYear = inventors.reduce((total, inventor) => {
+  return total + (inventor.passed - inventor.year);
+}, 0);
+console.log(totalYear);
+
+//Sort inventors by year lived
+const oldest = inventors.sort(function (a, b) {
+  const lastGuy = a.passed - a.year;
+  const nextGuy = b.passed - b.year;
+
+  if (lastGuy > nextGuy) {
+    return -1;
+  } else {
+    return 1;
+  }
+});
+console.table(oldest);
+
+//Create a list of Boulevands in Paris that contain 'de' anywhere in name
+//go to https://en.wikipedia.org/wiki/Category:Boulevands_in_Paris
+// Open Console and type thye following codes one by one
+// const category = document.querySelector('.mw-category');
+// const links = Array.from(category.querySelectorAll('a'));
+
+// const de = links.map(link => link.textContent).filter(streetName => streetName.includes('de'));
+
+//Sort the people in alplabetical order
+const alpOrder = people.sort((lastOne, nextOne) => {
+  const [aLast, aFirst] = lastOne.split(", ");
+  const [bLast, bFirst] = nextOne.split(", ");
+
+  return aLast > bLast ? 1 : -1;
+});
+console.log(alpOrder);
+
+//Sum of all instances usi9ng reduce
+const data = [
+  "car",
+  "car",
+  "truck",
+  "truck",
+  "bike",
+  "walk",
+  "car",
+  "van",
+  "bike",
+  "walk",
+  "car",
+  "van",
+  "car",
+  "truck",
+  "pogostick",
+];
+const transport = data.reduce(function(obj, item) {
+    if(!obj[item]) {
+        obj[item] = 0;
+    }
+    obj[item]++;
+    return obj;
+}, {});
+console.log(transport);
